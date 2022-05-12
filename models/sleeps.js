8204237299a420cb2550acfac2cfedd1467ab873
Dovydas Lozys <reduktorius@gmail.com>
2022-05-12 11:58:42 +0100

    first commit

---
+++ models/sleeps.js
@@ -0,0 +1,29 @@
+import format from "pg-format";
+import { pool } from "../db/connection.js";
+
+export async function getAllSleeps() {
+  const result = await pool.query(`SELECT * FROM sleeps;`);
+  return result.rows;
+}
+
+export async function getSleepsByChildID(childId, limit){
+  const result = await pool.query(
+    `SELECT * FROM sleeps WHERE child_id = $1 ORDER BY sleep_id DESC LIMIT $2;`, 
+    [childId, limit]
+  );
+  
+  return result.rows;
+}
+
+export async function createSleep(sleep) {
+  const sleep_arr = Object.values(sleep);
+
+  const result = await pool.query(
+    format(`INSERT INTO sleeps (
+      sleep_start, sleep_end, sleep_duration_ms, child_id, user_id)
+      VALUES (%L) RETURNING *;`, sleep_arr
+    )
+  );
+  
+  return result.rows;
+}
